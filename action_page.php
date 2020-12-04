10886e0a04eecede0ac83da4b17d3f00d70f4418
tejveersa <63759246+tejveersa@users.noreply.github.com>
2020-12-04 17:59:51 -0500

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Your subtotal is: <?php echo $_POST["fname"]; ?><br>
+With a gratituity of: <?php echo $_POST["lname"]; ?><br>
+Your total is: <?php echo $_POST["fname"*"lname"]; ?><br>
+
+</body>
+</html>
