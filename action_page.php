8b366761368d1a7a25f492a3798e548c69d9ed03
tejveersa <63759246+tejveersa@users.noreply.github.com>
2020-12-05 23:53:05 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,7 +1,11 @@
 <?php
+if( $_REQUEST["fname"]){
+  $sub_total = $_REQUEST["fname"]
+}
+if( $_REQUEST["lname"]){
+  $gratuity_rate = $_REQUEST["lname"]
+}
 
-$sub_total = $_GET['fname'];
-$gratuity_rate=$_GET['lname'];
 $gratuity=($sub_total*$gratuity_rate)/100;
 $total=$sub_total+$gratuity;
 echo("Gratuity Amount= $gratuity ");
